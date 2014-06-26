e1541fb6bf70bba67c568d726b8bb7761eb6744a
Richard Foge <fogeZombie@gmail.com>
2014-06-25 23:51:56 -0700

    Added a setup for Tables (route, model, controller, and template - no view yet).
    Got to Markup Chapter at ~21:00

--- js/app.js
+++ js/app.js
@@ -1,12 +1,39 @@
 App = Ember.Application.create();
 
-// for using fixture data
+// for using fixture data - new style
 // App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+// router
 App.Router.map(function() {
+	this.resource('tables'); // '/#/tables'
 });
 
+// routes
+App.TablesRoute = Ember.Route.extend({
+	model: function() {
+		return App.Table.find();
+	}
+});
 
+// controllers
+App.TablesController = Ember.ArrayController.extend();
+
+// models
+App.Store = DS.Store.extend({
+	revision: 11,
+	adapter: 'DS.FixtureAdapter'
+});
+
+App.Table = DS.Model.extend();
+
+App.Table.FIXTURES = [
+	{id: 1},
+	{id: 2},
+	{id: 3},
+	{id: 4},
+	{id: 5},
+	{id: 6}
+];
 
 // example of fixture data
 // App.<DataName> = DS.Model.extend({
