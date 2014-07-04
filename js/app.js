967cf45e32d49e506a724318b99eba92e257a3a6
Richard Foge <fogeZombie@gmail.com>
2014-07-03 22:04:59 -0700

    EmberFundamentals - Basic Data Interactions Added an IndexRoute and hooked up some hard-coded model data. Added a custom IndexController with a property (renderedOn) and an action (clicked) to demonstrate how they work.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,36 @@ App = Ember.Application.create({
 App.Router.map(function() {
 });
 
+// routes
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      "Richard Foge",
+      "Rob Conery",
+      "Scott Hanselman",
+      "Tom Dale",
+      "Yehuda Katz",
+      "Jon Galloway",
+      "Phil Hack"
+    ];
+  }
+});
+
+// controllers
+App.IndexController = Ember.ArrayController.extend({
+  renderedOn: function () {
+    return new Date();
+  }.property(),
+
+  actions: {
+    clicked: function () {
+      console.log("clicked!");
+    }
+  }
+});
+
+// models
+
 // example of fixture data
 // App.<DataName> = DS.Model.extend({
 // 	title: DS.attr('string'),
