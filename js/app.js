0c481d3cca812b2511562d370b55303d01ae2017
Richard Foge <fogeZombie@gmail.com>
2014-07-17 22:51:27 -0700

    Setup simple Users and User routes and templates. Using fixture data, FTW.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
 App = Ember.Application.create({
-  rootElement: "#user-admin-app"
+  rootElement: "#user-admin-app",
+  LOG_TRANSITIONS: true
 });
 
 // for using fixture data
-// App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+// url mapping
 App.Router.map(function() {
   this.resource('users');
+  this.resource('user', {path: "users/:id"});
 });
 
-// example of fixture data
-// App.<DataName> = DS.Model.extend({
-// 	title: DS.attr('string'),
-// 	isCompleted: DS.attr('boolean')
-// });
+// routes
+App.UsersRoute = Ember.Route.extend({
+  model: function() {
+    console.log("Users route hit up for model.");
+    return this.store.find('User');
+  }
+});
 
-// App.<DataName>.FIXTURES = [
-// 	{
-// 		id: 1,
-// 		title: "Learn Stuff",
-// 		isCompleted: false
-// 	}
-// ];
+App.UserRoute = Ember.Route.extend({
+  model: function(params) {
+    console.log("User route hit up for model.");
+    console.log(params);
+    return this.store.find('User', params.id);
+  }
+});
+
+// fixture data definition
+App.User = DS.Model.extend({
+  login: DS.attr('string'),
+  name: DS.attr('string'),
+	imageSource: DS.attr('string')
+});
+
+App.User.FIXTURES = [
+  {
+    id: "robconery",
+    login: "robconery",
+    name: "Rob Conery",
+    imageSource: "https://avatars.githubusercontent.com/u/78586?"
+  },
+  {
+    id: "scotthanselman",
+    login: "scotthanselman",
+    name: "Scott Hanselman",
+    imageSource: "https://avatars.githubusercontent.com/u/2892?"
+  }
+];
